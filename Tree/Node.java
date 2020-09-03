cd2eabb37afd7b8590193ba2467376b7fa2da727
Naveen Gaur <naveensgour@gmail.com>
2020-09-03 12:16:52 +0530

    Binary Tree 's few function implementation * Level order Insertion * Level order Traversal * Depth First Traversals: Inorder, Preorder, Postorder

---
+++ Tree/Node.java
@@ -0,0 +1,17 @@
+public class Node{
+    public
+    int data;
+    Node left, right;
+    public Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+    public Node()
+    {
+        this.data = 0;
+        left = null;
+        right = null;
+    }
+}
